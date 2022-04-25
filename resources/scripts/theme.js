dd4e3adfe32ace20cea0a0f8ef25815090968ba8
Abdiel Lopez <a.paperprototype@gmail.com>
2022-04-25 11:11:54 -0400

    fixed theme not saving

--- resources/scripts/theme.js
+++ resources/scripts/theme.js
@@ -1,57 +1,98 @@
 document.addEventListener("DOMContentLoaded", function() {
+	setTheme(); // inital page load
+
 	let toggle = document.getElementById("themeToggle");
 	toggle.addEventListener("click", function() {
-		let theme = document.getElementById("theme");
-		let themeText = document.getElementById("themeToggleText");
-
-		let savedTheme = sessionStorage.getItem("sparker/theme");
-
-		if (savedTheme === "")
-		{
-			if (theme.classList.contains("theme")) {
-				// set to dark
-				theme.classList.remove("theme");
-				theme.classList.add("theme-dark");
-
-				// set session storage
-				sessionStorage.setItem("sparker/theme", "dark");
-
-				console.log("theme:dark")
-				themeText.innerText = "Dark";
-			} else {
-				// set to auto
-				theme.classList.remove("theme-dark");
-				theme.classList.add("theme");
-
-				// set session storage
-				sessionStorage.setItem("sparker/theme", "auto");
-
-				console.log("theme:auto")
-				themeText.innerText = "Auto";
-			}
-		} else {
-			if (savedTheme === "auto") {
-				// set to dark
-				theme.classList.remove("theme");
-				theme.classList.add("theme-dark");
-
-				// set session storage
-				sessionStorage.setItem("sparker/theme", "dark");
-
-				console.log("theme:dark")
-				themeText.innerText = "Dark";
-			} else {
-				// set to auto
-				theme.classList.remove("theme-dark");
-				theme.classList.add("theme");
-
-				// set session storage
-				sessionStorage.setItem("sparker/theme", "auto");
-
-				console.log("theme:auto")
-				themeText.innerText = "Auto";
-			}
-		}
-
+		toggleTheme();
 	});
 });
+
+function toggleTheme() {
+	let theme = document.getElementById("theme");
+	let themeText = document.getElementById("themeToggleText");
+
+	let savedTheme = localStorage.getItem("sparker/theme");
+
+	if (savedTheme === "")
+	{
+		console.log("saved theme localStorage was empty");
+		if (theme.classList.contains("theme")) {
+			// set to dark
+			theme.classList.remove("theme");
+			theme.classList.add("theme-dark");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "dark");
+
+			console.log("theme:dark")
+			themeText.innerText = "Dark";
+		} else {
+			// set to auto
+			theme.classList.remove("theme-dark");
+			theme.classList.add("theme");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "auto");
+
+			console.log("theme:auto")
+			themeText.innerText = "Auto";
+		}
+	} else {
+		console.log("saved theme was not empty");
+		if (savedTheme === "auto") {
+			// set to dark
+			theme.classList.remove("theme");
+			theme.classList.add("theme-dark");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "dark");
+
+			console.log("theme:dark")
+			themeText.innerText = "Dark";
+		} else {
+			// set to auto
+			theme.classList.remove("theme-dark");
+			theme.classList.add("theme");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "auto");
+
+			console.log("theme:auto")
+			themeText.innerText = "Auto";
+		}
+	}
+}
+
+// set the theme to whatever localStorage setting says
+function setTheme() {
+	let theme = document.getElementById("theme");
+	let themeText = document.getElementById("themeToggleText");
+
+	let savedTheme = localStorage.getItem("sparker/theme");
+
+	// default leave as is
+	// otherwise 
+	if (savedTheme !== "") {
+		if (savedTheme === "auto") {
+			// set to auto
+			theme.classList.remove("theme-dark");
+			theme.classList.add("theme");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "auto");
+
+			console.log("theme:auto")
+			themeText.innerText = "Auto";
+		} else {
+			// set to dark
+			theme.classList.remove("theme");
+			theme.classList.add("theme-dark");
+
+			// set local storage
+			localStorage.setItem("sparker/theme", "dark");
+
+			console.log("theme:dark")
+			themeText.innerText = "Dark";
+		}
+	}
+}
