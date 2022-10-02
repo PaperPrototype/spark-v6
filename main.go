e422188d4cfba36f2ffeffccb06aa13691dbb6b1
Abdiel Lopez <a.paperprototype@gmail.com>
2022-10-02 12:36:38 -0500

    router2 is now default and only router (keeping old router for continued translation to new router2)

--- main.go
+++ main.go
@@ -5,45 +5,24 @@ import (
 	"main/db"
 	"main/mailer"
 	"main/payments"
-	"main/router"
 	"main/router2"
-	"os"
 )
 
 func main() {
-	args := os.Args
+	// new router
 
-	if len(args) > 1 {
-		// new router
+	// db connection pool used for course uploading
+	// and for performant db when ORM is too heavy
+	conn.Setup()
 
-		// db connection pool used for course uploading
-		// and for performant db when ORM is too heavy
-		conn.Setup()
+	mailer.Setup()
 
-		mailer.Setup()
+	payments.Setup()
 
-		payments.Setup()
+	db.Setup()
 
-		db.Setup()
+	router2.Run()
 
-		router2.Run()
+	conn.Close()
 
-		conn.Close()
-	} else {
-		// db connection pool used for course uploading
-		// and for performant db when ORM is too heavy
-		conn.Setup()
-
-		mailer.Setup()
-
-		payments.Setup()
-
-		router.Setup()
-
-		db.Setup()
-
-		router.Run()
-
-		conn.Close()
-	}
 }
